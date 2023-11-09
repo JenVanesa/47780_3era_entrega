3b7c99cc0f2ee32af076fc47e036d1bb28f8319d
Jenny V <jenycita_13@hotmail.com>
2023-11-09 01:07:03 -0500

    Agregado datos de HTML

--- fragancia/urls.py
+++ fragancia/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-from fragancia.views import (
-    listar_Perfume, listar_Cliente, listar_Compra
-)
+from fragancia.views import listar_Perfume, listar_Cliente, listar_Compra
+
 
 urlpatterns = [
     path("perfumes/", listar_Perfume),
